2a03cdb8936b9cc1da809506fad88bae2cbfb7dc
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2023-11-25 01:53:37 +0000

    Se implementa una lnción que split  divide una cadena en subcadenas utilizando un delimitador dado

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -12,3 +13,15 @@ struct Road {
     Road(const string& i, const string& c1, const string& c2, int co = -1) : id(i), city1(c1), city2(c2), cost(co) {}
 };
 
+vector<string> split(const string& str, char delim) {
+    vector<string> keys;
+    size_t start = 0;
+    size_t end = str.find(delim);
+    while (end != string::npos) {
+        keys.push_back(str.substr(start, end - start));
+        start = end + 1;
+        end = str.find(delim, start);
+    }
+    keys.push_back(str.substr(start));
+    return keys;
+}
